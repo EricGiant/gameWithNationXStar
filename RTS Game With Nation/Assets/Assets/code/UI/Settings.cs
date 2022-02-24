ade3e84d047a9de54beac5d6ef4f84748ab86694
Nationxstar <joshua.staedtke197@gmail.com>
2022-02-24 15:11:28 +0100

    Whack pause menu + settings + vibration settings

---
+++ RTS Game With Nation/Assets/Assets/code/UI/Settings.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Settings : MonoBehaviour
+{
+    private static bool GameisPaused = false;
+    private bool Vibration;
+    public Text VibrationText;
+    public void ChangeVibration(){
+        if (Vibration == true){
+            Vibration = false;
+            VibrationText.text = "Vibration: Off";        
+            Debug.Log("Vibration off");
+        }
+        else {
+            Vibration = true;
+            VibrationText.text = "Vibration: On";   
+            Debug.Log("Vibration on");
+        }
+    }
+    
+    public void Pause(){
+        if (GameisPaused)
+        {
+            Time.timeScale = 1f;
+            GameisPaused = false;
+        }
+        else 
+        {
+            Time.timeScale = 0f;
+            GameisPaused = true;
+        }
+    }
+}
