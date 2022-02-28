324ba7c656ee22050381120758bee531148a9b90
EricGiant <ericspier@hotmail.com>
2022-02-28 17:50:12 +0100

    tower vibrate on death

--- RTS Game With Nation/Assets/Assets/code/generale/objectStats.cs
+++ RTS Game With Nation/Assets/Assets/code/generale/objectStats.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class objectStats : MonoBehaviour
 {
     public float health;
+    public Vibrator vibrator;
 
     void Update()
     {
         if(health <= 0)
         {
+            if(!(vibrator == null))
+            {
+                vibrator.Vibrate();
+            }
             Destroy(gameObject);
         }
     }
