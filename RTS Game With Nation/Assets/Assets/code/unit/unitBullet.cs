8563e64abcef6072d2efe54e252e0ea281503fa4
EricGiant <ericspier@hotmail.com>
2022-03-04 17:08:43 +0100

    added back outcomented code

--- RTS Game With Nation/Assets/Assets/code/unit/unitBullet.cs
+++ RTS Game With Nation/Assets/Assets/code/unit/unitBullet.cs
@@ -22,34 +22,34 @@ public class unitBullet : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, goTo.position, moveSpeed);
         // // //for some weird reason the z axis is 0 for the unit bullet and -5 for the goto when it's a turret
         // // //so it will just look for x and y
-        // if(transform.position.x == goTo.position.x && transform.position.y == goTo.position.y)
-        // {
-        //     if(AI.name == "soldier" || AI.name == "humvee" || AI.name == "helicopter")
-        //     {
-        //         if(goTo.name == "soldier" || goTo.name == "rocketeer")
-        //         {
-        //             damage *= 1.5f;
-        //         }
-        //         else
-        //         {
-        //             damage *= 0.5f;
-        //         }
-        //     }
-        //     else
-        //     {
-        //         if(goTo.name == "soldier" || goTo.name == "rocketeer")
-        //         {
-        //             damage *= 0.5f;
-        //         }
-        //         else
-        //         {
-        //             damage *= 1.5f;
-        //         }
-        //     }
-        //     Debug.Log(AI.name + " dealt " + damage);
-        //     goTo.GetComponent<objectStats>().health -= damage;
-        //     Destroy(gameObject);
-        // }
+        if(transform.position.x == goTo.position.x && transform.position.y == goTo.position.y)
+        {
+            if(AI.name == "soldier" || AI.name == "humvee" || AI.name == "helicopter")
+            {
+                if(goTo.name == "soldier" || goTo.name == "rocketeer")
+                {
+                    damage *= 1.5f;
+                }
+                else
+                {
+                    damage *= 0.5f;
+                }
+            }
+            else
+            {
+                if(goTo.name == "soldier" || goTo.name == "rocketeer")
+                {
+                    damage *= 0.5f;
+                }
+                else
+                {
+                    damage *= 1.5f;
+                }
+            }
+            Debug.Log(AI.name + " dealt " + damage);
+            goTo.GetComponent<objectStats>().health -= damage;
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
