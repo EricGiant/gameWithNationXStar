33969c55f0e9313e719627ce9c9bd350a21f0326
EricGiant <ericspier@hotmail.com>
2022-03-05 17:23:50 +0100

    vic/def screen code

--- RTS Game With Nation/Assets/Assets/code/generale/objectStats.cs
+++ RTS Game With Nation/Assets/Assets/code/generale/objectStats.cs
@@ -7,6 +7,8 @@ public class objectStats : MonoBehaviour
     //holds health for every object in the game
     public float health;
     public Vibrator vibrator;
+    public bool isTownhall;
+    public GameObject vicScreen, defScreen;
 
     void Update()
     {
@@ -16,6 +18,21 @@ public class objectStats : MonoBehaviour
             if(!(vibrator == null))
             {
                 vibrator.Vibrate();
+            }
+            //checks if object is townhall when killed and then if victory/defeat screen has to pop up
+            if(isTownhall)
+            {
+                //check is townhall is player
+                //if it's player enable defeat screen
+                //if enemy enable victory screen
+                if(GetComponent<townHall>().isPlayer)
+                {
+                    defScreen.SetActive(true);
+                }
+                else
+                {
+                    vicScreen.SetActive(true);
+                }
             }
             Destroy(gameObject);
         }
